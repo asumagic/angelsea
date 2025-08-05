58f3ab57627279772120ebc31656845263576027
asu <sdelang@sdelang.fr>
2025-08-05 21:38:48 +0200

    Added iterative fib benchmark

--- tests/recursion.cpp
+++ tests/recursion.cpp
@@ -40,24 +40,35 @@ TEST_CASE("fib benchmark", "[fib][benchmark]") {
 
 	asIScriptFunction* jit_fib = jit_module.GetFunctionByDecl("int fib(int)");
 	ANGELSEA_TEST_CHECK(jit_fib != nullptr);
+	asIScriptFunction* jit_fib_iterative = jit_module.GetFunctionByDecl("uint64 fib_iterative(uint64)");
 
 	context.engine->SetEngineProperty(asEP_INCLUDE_JIT_INSTRUCTIONS, false);
 
 	asIScriptModule&   interp_module = context.build("build-nojit", "scripts/fib.as");
 	asIScriptFunction* interp_fib    = interp_module.GetFunctionByDecl("int fib(int)");
 	ANGELSEA_TEST_CHECK(interp_fib != nullptr);
+	asIScriptFunction* interp_fib_iterative = interp_module.GetFunctionByDecl("uint64 fib_iterative(uint64)");
 
 	asIScriptContext* script_context = context.engine->CreateContext();
 
-	const auto run_fib = [&](int i, asIScriptFunction* fib_fn) -> int {
+	const auto run_fib = [&](int i, asIScriptFunction* fib_fn, bool is_iterative) -> int {
 		ANGELSEA_TEST_CHECK(script_context->Prepare(fib_fn) >= 0);
-		ANGELSEA_TEST_CHECK(script_context->SetArgDWord(0, i) >= 0);
+		if (is_iterative) {
+			ANGELSEA_TEST_CHECK(script_context->SetArgQWord(0, i) >= 0);
+		} else {
+			ANGELSEA_TEST_CHECK(script_context->SetArgDWord(0, i) >= 0);
+		}
 		ANGELSEA_TEST_CHECK(script_context->Execute() == asEXECUTION_FINISHED);
 		return script_context->GetReturnDWord();
 	};
 
-	BENCHMARK("Interpreter fib(25)") { return run_fib(25, interp_fib); };
-	BENCHMARK("JIT         fib(25)") { return run_fib(25, jit_fib); };
+	ANGELSEA_TEST_CHECK(run_fib(10000, interp_fib_iterative, true) == run_fib(10000, jit_fib_iterative, true));
+
+	BENCHMARK("Interpreter fib(10000) (iterative)") { return run_fib(10000, interp_fib_iterative, true); };
+	BENCHMARK("JIT         fib(10000) (iterative)") { return run_fib(10000, jit_fib_iterative, true); };
+
+	BENCHMARK("Interpreter fib(25) (recursive)") { return run_fib(25, interp_fib, false); };
+	BENCHMARK("JIT         fib(25) (recursive)") { return run_fib(25, jit_fib, false); };
 }
 
 TEST_CASE("fib in a thread", "[fibasync]") {
--- tests/scripts/fib.as
+++ tests/scripts/fib.as
@@ -9,3 +9,19 @@ int fib(int n)
 
 	return fib(n-1) + fib(n-2);
 }
+
+/* it will, obviously, overflow very hard very often */
+uint64 fib_iterative(uint64 n)
+{
+	uint64 last = 1;
+	uint64 prev = 0;
+
+	for (uint64 i = 1; i < n; ++i)
+	{
+		uint64 next = prev + last;
+		prev = last;
+		last = next;
+	}
+
+	return last;
+}
