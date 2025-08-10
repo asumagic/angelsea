ae13b4fc4762266893f4a1e4bb8fbd9392b937a4
asu <sdelang@sdelang.fr>
2025-08-10 19:01:38 +0200

    Further document SetCompileCallback

--- include/angelsea/jit.hpp
+++ include/angelsea/jit.hpp
@@ -33,6 +33,14 @@ class Jit final : public asIJITCompilerV2 {
 	/// Configure a compilation callback for asynchronous/threaded compilation.
 	/// Your callback will be called with a callable function pointer, which you must invoke with the provided void*
 	/// user data parameter, wherever you see fit. This can be in a thread pool or a dedicated thread, for instance.
+	///
+	/// Compile times are typically not very long, but long enough to be an issue for realtime applications.
+	/// When lazy compilation triggers a function compile, a compile task will be spawned. Once the compile is finished,
+	/// and the main thread running the script is hitting a JitEntry again, the script function will be patched to allow
+	/// jumping into the JIT.
+	///
+	/// For the time being, compile tasks can lock mutexes for heavy tasks, and thus block the thread for a fairly long
+	/// amount of time. Hence, it is discouraged to make compile jobs happen in the same background pool as other tasks.
 	void SetCompileCallback(std::function<void(CompileFunc*, void*)> callback);
 
 	private:
