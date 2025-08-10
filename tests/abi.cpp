982faaa5ff4db5c215e9eb3beb24e26c509a0472
asu <sdelang@sdelang.fr>
2025-08-10 17:42:35 +0200

    Refactor abi benchmark

--- tests/abi.cpp
+++ tests/abi.cpp
@@ -9,6 +9,8 @@
 
 struct SomeClass {
 	int a = 0;
+
+	void add_obj(int x) { a += x; }
 };
 
 struct NoisyClass {
@@ -51,21 +53,16 @@ void generic_return_string(asIScriptGeneric* gen) {
 void generic_take_noisy(asIScriptGeneric* gen) {}
 
 void bind_generic_functions(asIScriptEngine& e) {
-	e.RegisterGlobalFunction("int generic_noarg()", asFUNCTION(generic_noarg_hack), asCALL_GENERIC);
-	e.RegisterGlobalFunction("int generic_sum3int(int, int, int)", asFUNCTION(generic_sum3int), asCALL_GENERIC);
+	e.RegisterGlobalFunction("int noarg()", asFUNCTION(generic_noarg_hack), asCALL_GENERIC);
+	e.RegisterGlobalFunction("int sum3int(int, int, int)", asFUNCTION(generic_sum3int), asCALL_GENERIC);
 	e.RegisterGlobalFunction(
-	    "string generic_return_string(int, int, const string&in)",
+	    "string return_string(int, int, const string&in)",
 	    asFUNCTION(generic_return_string),
 	    asCALL_GENERIC
 	);
 
 	e.RegisterObjectType("SomeClass", sizeof(SomeClass), asOBJ_VALUE | asOBJ_POD);
-	e.RegisterObjectMethod(
-	    "SomeClass",
-	    "void generic_add_obj(int x)",
-	    asFUNCTION(generic_add_obj_hack),
-	    asCALL_GENERIC
-	);
+	e.RegisterObjectMethod("SomeClass", "void add_obj(int x)", asFUNCTION(generic_add_obj_hack), asCALL_GENERIC);
 	e.RegisterObjectProperty("SomeClass", "int a", asOFFSET(SomeClass, a));
 
 	e.RegisterObjectType("NoisyClass", sizeof(SomeClass), asOBJ_VALUE | asGetTypeTraits<NoisyClass>());
@@ -80,7 +77,7 @@ void bind_generic_functions(asIScriptEngine& e) {
 	e.RegisterObjectBehaviour("NoisyClass", asBEHAVE_DESTRUCT, "void f()", WRAP_DES(NoisyClass), asCALL_GENERIC);
 
 	e.RegisterGlobalFunction(
-	    "void generic_take_noisy(NoisyClass a, NoisyClass b)",
+	    "void take_noisy(NoisyClass a, NoisyClass b)",
 	    asFUNCTION(generic_take_noisy),
 	    asCALL_GENERIC
 	);
@@ -90,19 +87,19 @@ TEST_CASE("generic calling convention", "[abi][conv_generic]") {
 	EngineContext context;
 	bind_generic_functions(*context.engine);
 
-	REQUIRE(run_string(context, "print(''+generic_noarg())") == "123\n");
+	REQUIRE(run_string(context, "print(''+noarg())") == "123\n");
 
 	// involves the stack pointer
-	REQUIRE(run_string(context, "print(''+generic_sum3int(500, 30, 2))") == "532\n");
+	REQUIRE(run_string(context, "print(''+sum3int(500, 30, 2))") == "532\n");
 
 	// involves the object pointer
-	REQUIRE(run_string(context, "SomeClass s; s.a = 0; s.generic_add_obj(100); print(''+s.a);") == "100\n");
+	REQUIRE(run_string(context, "SomeClass s; s.a = 0; s.add_obj(100); print(''+s.a);") == "100\n");
 
 	// involves a return on stack, but no cleanargs or otherwise
-	REQUIRE(run_string(context, "print(generic_return_string(123, 456, ' :3'));") == "hello world! 123, 456 :3\n");
+	REQUIRE(run_string(context, "print(return_string(123, 456, ' :3'));") == "hello world! 123, 456 :3\n");
 
 	// involves cleanargs
-	REQUIRE(run_string(context, "generic_take_noisy(NoisyClass(), NoisyClass());") == "ConCpyDesConCpyDesDesDes");
+	REQUIRE(run_string(context, "take_noisy(NoisyClass(), NoisyClass());") == "ConCpyDesConCpyDesDesDes");
 }
 
 TEST_CASE("generic abi benchmark", "[abi][conv_generic][benchmark]") {
@@ -118,15 +115,14 @@ TEST_CASE("generic abi benchmark", "[abi][conv_generic][benchmark]") {
 	};
 
 	asIScriptModule&   jit_module               = context.build("build", "scripts/abi.as");
-	asIScriptFunction& jit_million_bench        = get_bench(jit_module, "void benchmark_1M_calls_generic_noarg()");
-	asIScriptFunction& jit_million_method_bench = get_bench(jit_module, "void benchmark_1M_calls_generic_method_arg()");
+	asIScriptFunction& jit_million_bench        = get_bench(jit_module, "void benchmark_1M_calls_noarg()");
+	asIScriptFunction& jit_million_method_bench = get_bench(jit_module, "void benchmark_1M_calls_method_arg()");
 	context.prepare_execution();
 
 	context.engine->SetEngineProperty(asEP_INCLUDE_JIT_INSTRUCTIONS, false);
-	asIScriptModule&   interp_module        = context.build("build-nojit", "scripts/abi.as");
-	asIScriptFunction& interp_million_bench = get_bench(interp_module, "void benchmark_1M_calls_generic_noarg()");
-	asIScriptFunction& interp_million_method_bench
-	    = get_bench(interp_module, "void benchmark_1M_calls_generic_method_arg()");
+	asIScriptModule&   interp_module               = context.build("build-nojit", "scripts/abi.as");
+	asIScriptFunction& interp_million_bench        = get_bench(interp_module, "void benchmark_1M_calls_noarg()");
+	asIScriptFunction& interp_million_method_bench = get_bench(interp_module, "void benchmark_1M_calls_method_arg()");
 
 	asIScriptContext* script_context = context.engine->CreateContext();
 
@@ -147,17 +143,89 @@ int native_noarg() { return 123; }
 
 int native_sum3int(int a, int b, int c) { return a + b + c; }
 
+std::string return_string(int a, int b, const std::string& c) {
+	std::string ret("hello world! ");
+	ret += std::to_string(a);
+	ret += ", ";
+	ret += std::to_string(b);
+	ret += c;
+	return ret;
+}
+
 void bind_native_functions(asIScriptEngine& e) {
-	e.RegisterGlobalFunction("int native_noarg()", asFUNCTION(native_noarg), asCALL_CDECL);
-	e.RegisterGlobalFunction("int native_sum3int(int, int, int)", asFUNCTION(native_sum3int), asCALL_CDECL);
+	e.RegisterGlobalFunction("int noarg()", asFUNCTION(native_noarg), asCALL_CDECL);
+	e.RegisterGlobalFunction("int sum3int(int, int, int)", asFUNCTION(native_sum3int), asCALL_CDECL);
+	e.RegisterGlobalFunction(
+	    "string return_string(int, int, const string&in)",
+	    asFUNCTION(generic_return_string),
+	    asCALL_GENERIC
+	);
+
+	e.RegisterObjectType("SomeClass", sizeof(SomeClass), asOBJ_VALUE | asOBJ_POD);
+	e.RegisterObjectMethod("SomeClass", "void add_obj(int x)", asMETHOD(SomeClass, add_obj), asCALL_THISCALL);
+	e.RegisterObjectProperty("SomeClass", "int a", asOFFSET(SomeClass, a));
+
+	e.RegisterObjectType("NoisyClass", sizeof(SomeClass), asOBJ_VALUE | asGetTypeTraits<NoisyClass>());
+	e.RegisterObjectBehaviour("NoisyClass", asBEHAVE_CONSTRUCT, "void f()", WRAP_CON(NoisyClass, ()), asCALL_GENERIC);
+	e.RegisterObjectBehaviour(
+	    "NoisyClass",
+	    asBEHAVE_CONSTRUCT,
+	    "void f(const NoisyClass &in)",
+	    WRAP_CON(NoisyClass, (const NoisyClass&)),
+	    asCALL_GENERIC
+	);
+	e.RegisterObjectBehaviour("NoisyClass", asBEHAVE_DESTRUCT, "void f()", WRAP_DES(NoisyClass), asCALL_GENERIC);
+
+	e.RegisterGlobalFunction(
+	    "void take_noisy(NoisyClass a, NoisyClass b)",
+	    asFUNCTION(generic_take_noisy),
+	    asCALL_GENERIC
+	);
 }
 
 TEST_CASE("native calling convention", "[abi][conv_native]") {
 	EngineContext context;
 	bind_native_functions(*context.engine);
 
-	REQUIRE(run_string(context, "print(''+native_noarg())") == "123\n");
+	REQUIRE(run_string(context, "print(''+noarg())") == "123\n");
 
 	// involves the stack pointer
-	REQUIRE(run_string(context, "print(''+native_sum3int(500, 30, 2))") == "532\n");
+	REQUIRE(run_string(context, "print(''+sum3int(500, 30, 2))") == "532\n");
+}
+
+TEST_CASE("native abi benchmark", "[abi][conv_native][benchmark]") {
+	EngineContext context;
+	bind_native_functions(*context.engine);
+
+	out = {};
+
+	auto get_bench = [](asIScriptModule& module, const char* decl) -> asIScriptFunction& {
+		asIScriptFunction* fn = module.GetFunctionByDecl(decl);
+		ANGELSEA_TEST_CHECK(fn != nullptr);
+		return *fn;
+	};
+
+	asIScriptModule&   jit_module               = context.build("build", "scripts/abi.as");
+	asIScriptFunction& jit_million_bench        = get_bench(jit_module, "void benchmark_1M_calls_noarg()");
+	asIScriptFunction& jit_million_method_bench = get_bench(jit_module, "void benchmark_1M_calls_method_arg()");
+	context.prepare_execution();
+
+	context.engine->SetEngineProperty(asEP_INCLUDE_JIT_INSTRUCTIONS, false);
+	asIScriptModule&   interp_module               = context.build("build-nojit", "scripts/abi.as");
+	asIScriptFunction& interp_million_bench        = get_bench(interp_module, "void benchmark_1M_calls_noarg()");
+	asIScriptFunction& interp_million_method_bench = get_bench(interp_module, "void benchmark_1M_calls_method_arg()");
+
+	asIScriptContext* script_context = context.engine->CreateContext();
+
+	const auto run_fn = [&](asIScriptFunction& fn) -> int {
+		ANGELSEA_TEST_CHECK(script_context->Prepare(&fn) >= 0);
+		ANGELSEA_TEST_CHECK(script_context->Execute() == asEXECUTION_FINISHED);
+		return script_context->GetReturnDWord();
+	};
+
+	BENCHMARK("JIT    1M native `void(this, int)`") { return run_fn(jit_million_method_bench); };
+	BENCHMARK("Interp 1M native `void(this, int)`") { return run_fn(interp_million_method_bench); };
+
+	BENCHMARK("JIT    1M native `int()`") { return run_fn(jit_million_bench); };
+	BENCHMARK("Interp 1M native `int()`") { return run_fn(interp_million_bench); };
 }
--- tests/scripts/abi.as
+++ tests/scripts/abi.as
@@ -1,14 +1,14 @@
-void benchmark_1M_calls_generic_noarg() {
+void benchmark_1M_calls_noarg() {
     for (int i = 0; i < 1000000; ++i) {
-        generic_noarg();
+        noarg();
     }
 }
 
-void benchmark_1M_calls_generic_method_arg() {
+void benchmark_1M_calls_method_arg() {
     SomeClass s;
     s.a = 0;
     for (int i = 0; i < 1000000; ++i) {
-        s.generic_add_obj(i);
+        s.add_obj(i);
     }
     print('' + s.a);
 }
