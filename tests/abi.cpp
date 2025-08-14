df8dc8939808654a8619b3c88de61742daef000f
asu <sdelang@sdelang.fr>
2025-08-14 15:00:53 +0200

    Add native test with many arguments

--- tests/abi.cpp
+++ tests/abi.cpp
@@ -144,6 +144,12 @@ int native_noarg() { return 123; }
 int native_sum3int(int a, int b, int c) { return a + b + c; }
 int native_sum3float(float a, float b, float c) { return a + b + c; }
 
+// clang-format off
+int native_manyargs(int x0, int x1, int x2, int x3, int x4, int x5, int x6, int x7, int x8, float y1, float y2, float y3, float y4, int x9, int x10, int x11, int x12, int x13, int x14, int x15, int x16) { // clang-format on
+	return x0 + x1 + x2 + x3 + x4 + x5 + x6 + x7 + x8 + (y1 + y2 + y3 + y4) + x9 + x10 + x11 + x12 + x13 + x14 + x15
+	    + x16;
+}
+
 std::string return_string(int a, int b, const std::string& c) {
 	std::string ret("hello world! ");
 	ret += std::to_string(a);
@@ -167,21 +173,28 @@ void bind_native_functions(asIScriptEngine& e) {
 	e.RegisterObjectMethod("SomeClass", "void add_obj(int x)", asMETHOD(SomeClass, add_obj), asCALL_THISCALL);
 	e.RegisterObjectProperty("SomeClass", "int a", asOFFSET(SomeClass, a));
 
-	e.RegisterObjectType("NoisyClass", sizeof(SomeClass), asOBJ_VALUE | asGetTypeTraits<NoisyClass>());
-	e.RegisterObjectBehaviour("NoisyClass", asBEHAVE_CONSTRUCT, "void f()", WRAP_CON(NoisyClass, ()), asCALL_GENERIC);
-	e.RegisterObjectBehaviour(
-	    "NoisyClass",
-	    asBEHAVE_CONSTRUCT,
-	    "void f(const NoisyClass &in)",
-	    WRAP_CON(NoisyClass, (const NoisyClass&)),
-	    asCALL_GENERIC
-	);
-	e.RegisterObjectBehaviour("NoisyClass", asBEHAVE_DESTRUCT, "void f()", WRAP_DES(NoisyClass), asCALL_GENERIC);
+	// e.RegisterObjectType("NoisyClass", sizeof(SomeClass), asOBJ_VALUE | asGetTypeTraits<NoisyClass>());
+	// e.RegisterObjectBehaviour("NoisyClass", asBEHAVE_CONSTRUCT, "void f()", WRAP_CON(NoisyClass, ()),
+	// asCALL_GENERIC); e.RegisterObjectBehaviour(
+	//     "NoisyClass",
+	//     asBEHAVE_CONSTRUCT,
+	//     "void f(const NoisyClass &in)",
+	//     WRAP_CON(NoisyClass, (const NoisyClass&)),
+	//     asCALL_GENERIC
+	// );
+	// e.RegisterObjectBehaviour("NoisyClass", asBEHAVE_DESTRUCT, "void f()", WRAP_DES(NoisyClass), asCALL_GENERIC);
+
+	// e.RegisterGlobalFunction(
+	//     "void take_noisy(NoisyClass a, NoisyClass b)",
+	//     asFUNCTION(generic_take_noisy),
+	//     asCALL_GENERIC
+	// );
 
 	e.RegisterGlobalFunction(
-	    "void take_noisy(NoisyClass a, NoisyClass b)",
-	    asFUNCTION(generic_take_noisy),
-	    asCALL_GENERIC
+	    "int native_manyargs(int x0, int x1, int x2, int x3, int x4, int x5, int x6, int x7, int x8, float y1, float "
+	    "y2, float y3, float y4, int x9, int x10, int x11, int x12, int x13, int x14, int x15, int x16)",
+	    asFUNCTION(native_manyargs),
+	    asCALL_CDECL
 	);
 }
 
@@ -195,6 +208,14 @@ TEST_CASE("native calling convention", "[abi][conv_native]") {
 	REQUIRE(run_string(context, "print(''+sum3int(500, 30, 2))") == "532\n");
 
 	REQUIRE(run_string(context, "print(''+sum3float(500.5, 29.5, 2))") == "532\n");
+
+	REQUIRE(
+	    run_string(
+	        context,
+	        "print(''+native_manyargs(0, 1, 2, 3, 4, 5, 6, 7, 8, 0.5, 1.5, 3.0, 6.0, 9, 10, 11, 12, 13, 14, 15, 16))"
+	    )
+	    == "147\n"
+	);
 }
 
 TEST_CASE("native abi benchmark", "[abi][conv_native][benchmark]") {
