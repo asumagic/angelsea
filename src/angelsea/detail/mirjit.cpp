ca435ea8a6d8f80dc61bac76f518803d930f4162
asu <sdelang@sdelang.fr>
2025-08-23 01:48:58 +0200

    Build fix on aarch64 macOS

--- src/angelsea/detail/mirjit.cpp
+++ src/angelsea/detail/mirjit.cpp
@@ -274,13 +274,13 @@ void MirJit::codegen_async_function(AsyncMirFunction& fn) {
 	{
 		C2Mir c2mir{compile_mir};
 
-		std::array<c2mir_macro_command, 1> macros{{
+		std::array macros{
 		    // Trigger the various definitions and macros of the generated header
-		    {.def_p = int(true), .name = "ASEA_SUPPORT", .def = "1"},
+		    c2mir_macro_command{.def_p = int(true), .name = "ASEA_SUPPORT", .def = "1"},
 #ifdef AS_ARM64
-		    {.def_p = int(true), .name = "AS_ARM64", .def = "1"},
+		    c2mir_macro_command{.def_p = int(true), .name = "AS_ARM64", .def = "1"},
 #endif
-		}};
+		};
 
 		c2mir_options c_options{
 		    .message_file       = config().debug.c2mir_diagnostic_file,
