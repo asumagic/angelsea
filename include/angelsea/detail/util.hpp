a7a0f484fe99809ec8a17f54883b8a3e8c9ab4af
asu <sdelang@sdelang.fr>
2025-08-22 17:00:06 +0200

    Tentative fix for clang build fail

--- include/angelsea/detail/util.hpp
+++ include/angelsea/detail/util.hpp
@@ -8,4 +8,7 @@ template<class... Ts> struct overloaded : Ts... {
 	using Ts::operator()...;
 };
 
+// shouldn't be needed with C++20 but the clang version on ubuntu 22.04 seems to dislike not having it
+template<class... Ts> overloaded(Ts...) -> overloaded<Ts...>;
+
 } // namespace angelsea::detail
