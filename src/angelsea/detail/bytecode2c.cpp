41836d0750e7bf4d7daaf052a0e7f48a43b06c87
asu <sdelang@sdelang.fr>
2025-08-23 02:44:01 +0200

    Fix attempt for baseOffset on aarch64 macOS

--- src/angelsea/detail/bytecode2c.cpp
+++ src/angelsea/detail/bytecode2c.cpp
@@ -1917,10 +1917,8 @@ BytecodeToC::SystemCallEmitResult BytecodeToC::emit_direct_system_call_native(
 	}
 
 	if (sys_fn.baseOffset > 0) {
-		// we preserve the arm/aarch64 check from AS here but it is untested!
 		emit(
-		    "#if ((defined(__GNUC__) || defined(__MIRC__)) && (defined(AS_ARM64) || defined(AS_ARM) || "
-		    "defined(AS_MIPS))) || defined(AS_PSVITA)\n"
+		    "#if ((defined(__GNUC__) || defined(__MIRC__)) && defined(__aarch64__)\n"
 		    "\t\tobj = (char*)obj + {BASE_OFFSET} >> 1;\n"
 		    "#else\n"
 		    "\t\tobj = (char*)obj + {BASE_OFFSET};\n"
